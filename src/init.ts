dfe341c88503281926666d5a3d2e97a54ce92456
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-10-12 12:17:31 +0900

    feat: update scripts

--- src/init.ts
+++ src/init.ts
@@ -1,1 +1,2 @@
 import "ress";
+import "./styles/style.css";
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,77 @@
-import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
+import { Engine, Render, Runner, Bodies, Composite, Mouse, MouseConstraint, Vector, Body } from "matter-js";
+const THICKNESS = 60;
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-    
-  </div>
-`;
+const engine = Engine.create();
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+const render = Render.create({
+  element: document.getElementById("app")!,
+  engine: engine,
+  options: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    background: "#00BFD6",
+    wireframes: false,
+    showAngleIndicator: true,
+  },
+});
+
+const mouse = Mouse.create(render.canvas);
+
+const mouseConstraint = MouseConstraint.create(engine, {
+  mouse: mouse,
+  constraint: {
+    stiffness: 0.2,
+    render: {
+      visible: false,
+    },
+  },
+});
+
+Composite.add(engine.world, mouseConstraint);
+
+let boxA = Bodies.rectangle(400, 200, 80, 80);
+let boxB = Bodies.rectangle(450, 50, 80, 80);
+
+for (let i = 0; i < 10; i++) {
+  const circle = Bodies.circle(i, 10, 30, {
+    friction: 0.3,
+    frictionAir: 0.00001,
+    restitution: 0.8,
+  });
+  Composite.add(engine.world, circle);
+}
+
+let ground = Bodies.rectangle(window.innerWidth / 2, window.innerHeight + THICKNESS / 2, 27184, THICKNESS, {
+  isStatic: true,
+});
+
+let leftWall = Bodies.rectangle(0 - THICKNESS / 2, window.innerHeight / 2, THICKNESS, window.innerHeight * 10, {
+  isStatic: true,
+});
+let rightWall = Bodies.rectangle(
+  window.innerWidth + THICKNESS / 2,
+  window.innerHeight / 2,
+  THICKNESS,
+  window.innerHeight * 10,
+  {
+    isStatic: true,
+  }
+);
+
+Composite.add(engine.world, [boxA, boxB, ground, leftWall, rightWall]);
+render.mouse = mouse;
+
+function handleResize() {
+  render.canvas.width = window.innerWidth;
+  render.canvas.height = window.innerHeight;
+  Body.setPosition(ground, Vector.create(window.innerWidth / 2, window.innerHeight + THICKNESS / 2));
+  Body.setPosition(leftWall, Vector.create(0 - THICKNESS / 2, window.innerHeight / 2));
+  Body.setPosition(rightWall, Vector.create(window.innerWidth + THICKNESS / 2, window.innerHeight / 2));
+}
+
+window.addEventListener("resize", handleResize);
+
+const runner = Runner.create();
+
+Render.run(render);
+Runner.run(runner, engine);
