b36c2479bd7d62a617899037ce555f8d62e3bf67
Ryoga-exe <git@ryoga.dev>
2024-01-06 03:25:57 +0900

    feat: add poly-decomp and pathseg

--- src/init.ts
+++ src/init.ts
@@ -1,2 +1,7 @@
 import "ress";
 import "./styles/style.css";
+// @ts-expect-error
+import decomp from "poly-decomp";
+import { Common } from "matter-js";
+Common.setDecomp(decomp);
+import "pathseg";
